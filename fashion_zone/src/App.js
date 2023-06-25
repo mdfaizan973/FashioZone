f04c1219b36bf82cdcfe338e7af04375d01eaf16
mdfaizan973 <faizan.md9735@gmail.com>
2023-06-25 11:57:09 +0530

    able to make routes

--- fashion_zone/src/App.js
+++ fashion_zone/src/App.js
@@ -1,10 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import AllRoutes from "./Components/AllRoutes";
 
 function App() {
   return (
     <div className="App">
-      <h1>FashionZone</h1>
+      <AllRoutes />
     </div>
   );
 }
---
+++ fashion_zone/src/Pages/ProDetails.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ProDetails() {
+  return <div>ProDetails</div>;
+}
