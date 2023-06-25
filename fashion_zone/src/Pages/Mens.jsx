78e2087d5d1000b6a9de4cd0b92496e67737811f
mdfaizan973 <faizan.md9735@gmail.com>
2023-06-25 18:42:21 +0530

    able to make single page

--- fashion_zone/src/Pages/Mens.jsx
+++ fashion_zone/src/Pages/Mens.jsx
@@ -1,26 +1,14 @@
 import React from "react";
 import "./Styles/Mens.css";
+import { Link as RouterLink } from "react-router-dom";
 export default function Mens() {
-  let arrays = [1, 2, 3, 4];
-  return (
-    <div id="maincontainer">
-      <div id="pdoducts">
-        {arrays.map((ele, i) => (
-          <ProductsCard />
-        ))}
-      </div>
-    </div>
-  );
-}
-
-function ProductsCard() {
   const products = [
     {
       name: "Product 1",
       price: 9.99,
       description: "Lorem ipsum dolor sit amet.",
       image:
-        "https://m.media-amazon.com/images/I/41hEm7yG6oL._AC_UL600_FMwebp_QL65_.jpg",
+        "https://rukminim1.flixcart.com/image/832/832/xif0q/t-shirt/r/i/v/xxl-aks-0150-ausk-original-imagk4nyd4c4hwug.jpeg?q=70",
     },
     {
       name: "Product 2",
@@ -29,32 +17,57 @@ function ProductsCard() {
       image:
         "https://m.media-amazon.com/images/I/41hEm7yG6oL._AC_UL600_FMwebp_QL65_.jpg",
     },
+    {
+      name: "Product 2",
+      price: 14.99,
+      description: "Lorem ipsum dolor sit amet consectetur.",
+      image: "https://m.media-amazon.com/images/I/71V0UjkEnpL._UY679_.jpg",
+    },
+    {
+      name: "Product 2",
+      price: 14.99,
+      description: "Lorem ipsum dolor sit amet consectetur.",
+      image:
+        "https://rukminim1.flixcart.com/image/832/832/xif0q/t-shirt/j/5/c/m-mennevypenalnew-dark-culture-original-imagktwhc4nzs8qu.jpeg?q=70",
+    },
   ];
   return (
-    <div id="productscards">
-      <div class="flipkart-card">
-        <img
-          src="https://m.media-amazon.com/images/I/71V0UjkEnpL._UY679_.jpg"
-          alt="Product Image"
-          class="product-image"
-        />
-        <p class="product-title">Peter England University </p>
-        <p class="product-price">$99.99</p>
-        <div class="product-rating">
-          <span class="star">&#9733;</span>
-          <span class="star">&#9733;</span>
-          <span class="star">&#9733;</span>
-          <span class="star">&#9734;</span>
-          <span class="star">&#9734;</span>
-        </div>
-        <p class="product-description">
-          Product Description Lorem ipsum dolor sit amet, consectetur adipiscing
-          elit.
-        </p>
-        <button class="add-to-cart">
-          <i class="fas fa-heart"></i>Add to Cart
-        </button>
+    <div id="maincontainer">
+      <div id="pdoducts">
+        {products.map((ele, i) => (
+          <ProductsCard data={ele} />
+        ))}
       </div>
+    </div>
+  );
+}
+
+function ProductsCard(data) {
+  console.log(data.data.name);
+  return (
+    <div id="productscards">
+      <RouterLink to={`/details/:${1}`}>
+        <div className="flipkart-card">
+          <img
+            src={data.data.image}
+            alt="Product Image"
+            className="product-image"
+          />
+          <p className="product-title">{data.data.name}</p>
+          <p className="product-price">${data.data.price}</p>
+          <div className="product-rating">
+            <span className="star">&#9733;</span>
+            <span className="star">&#9733;</span>
+            <span className="star">&#9733;</span>
+            <span className="star">&#9734;</span>
+            <span className="star">&#9734;</span>
+          </div>
+          <p className="product-description">{data.data.description}</p>
+          <button className="add-to-cart">
+            <i className="fas fa-heart"></i>Add to Cart
+          </button>
+        </div>
+      </RouterLink>
     </div>
   );
 }
--- fashion_zone/src/Pages/ProDetails.jsx
+++ fashion_zone/src/Pages/ProDetails.jsx
@@ -1,5 +1,67 @@
 import React from "react";
-
+import "./Styles/Details.css";
 export default function ProDetails() {
-  return <div>ProDetails</div>;
+  return (
+    <div>
+      <div class="product-details">
+        <div class="product-header">
+          <div>
+            <h1 class="product-title">Product Title</h1>
+            <p class="product-price">$99.99</p>
+          </div>
+          <img
+            src="https://m.media-amazon.com/images/I/71V0UjkEnpL._UY679_.jpg"
+            alt="Product Image"
+            class="product-image top-image"
+          />
+        </div>
+        <div class="product-images">
+          <img
+            src="https://m.media-amazon.com/images/I/71V0UjkEnpL._UY679_.jpg"
+            alt="Product Image 1"
+          />
+          <img
+            src="https://m.media-amazon.com/images/I/71V0UjkEnpL._UY679_.jpg"
+            alt="Product Image 2"
+          />
+          <img
+            src="https://m.media-amazon.com/images/I/71V0UjkEnpL._UY679_.jpg"
+            alt="Product Image 3"
+          />
+        </div>
+        <div class="product-description">
+          <h2>Description</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+            tincidunt enim vel lorem vulputate vestibulum.
+          </p>
+        </div>
+        <div class="product-rating">
+          <h2>Rating</h2>
+          <p>4.5 out of 5 stars</p>
+        </div>
+        <div class="product-features">
+          <h2>Key Features</h2>
+          <ul class="feature-list">
+            <li>Feature 1</li>
+            <li>Feature 2</li>
+            <li>Feature 3</li>
+          </ul>
+        </div>
+        <div class="product-details-section">
+          <h2>Specifications</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div class="product-details-section">
+          <h2>Package Includes</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div class="product-details-section">
+          <h2>Customer Reviews</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <button class="add-to-cart">Add to Cart</button>
+      </div>
+    </div>
+  );
 }
