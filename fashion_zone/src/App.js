c9bce0f4f8a0e2939e778d7ee122fb507377470b
mdfaizan973 <faizan.md9735@gmail.com>
2023-06-25 11:40:41 +0530

    start project

---
+++ fashion_zone/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>FashionZone</h1>
+    </div>
+  );
+}
+
+export default App;
