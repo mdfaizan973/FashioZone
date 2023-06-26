3b95d060f252f9b069479307e7a7b78f9999f344
mdfaizan973 <faizan.md9735@gmail.com>
2023-06-26 18:06:01 +0530

    able to total

--- fashion_zone/src/Pages/Cart.jsx
+++ fashion_zone/src/Pages/Cart.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Styles/Carts.css";
 import { BiCartAlt, BiBadgeCheck } from "react-icons/bi";
+import axios from "axios";
 export default function Cart() {
-  let cart_arr = [1, 2, 3];
+  const [cart_pro, setCart_Pro] = useState([]);
+  const [tprice, setTprice] = useState(0);
+  const getcartdata = () => {
+    axios
+      .get(`http://localhost:8888/cart`)
+      .then((res) => {
+        setCart_Pro(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    getcartdata();
+  }, []);
+
+  let sum = 0;
+  for (let elem of cart_pro) {
+    sum += +elem.price;
+  }
   return (
     <div>
       <div className="cart-page">
@@ -15,20 +35,15 @@ export default function Cart() {
         >
           My Cart - <BiCartAlt />
         </h1>
-        {cart_arr.map((ele, i) => (
-          <div className="cart-item">
+        {cart_pro.map((ele, i) => (
+          <div key={ele.id} className="cart-item">
             <div className="cart-item__image">
-              <img
-                src="https://rukminim1.flixcart.com/image/832/832/xif0q/shirt/b/3/2/xxl-st10-vebnor-original-imagzdu7azhy9tvf.jpeg?q=70"
-                alt="Product Image"
-              />
+              <img src={ele.img1} alt="Product Image" />
             </div>
             <div className="cart-item__details">
-              <div className="cart-item__name">TShirt</div>
-              <div className="cart-item__title">
-                Men Striped Round Neck Cotton Blend Blue T-Shirt
-              </div>
-              <div className="cart-item__price">$10.00</div>
+              <div className="cart-item__name">{ele.name}</div>
+              <div className="cart-item__title">{ele.title}</div>
+              <div className="cart-item__price">${ele.price}</div>
               <div className="cart-item__quantity">
                 <div className="cart-item__quantity-controls">
                   <button>-</button>
@@ -41,9 +56,9 @@ export default function Cart() {
           </div>
         ))}
         <div className="cart-page__subtotal">
-          Total Quantity: {cart_arr.length}
+          Total Quantity: {cart_pro.length}
         </div>
-        <div className="cart-page__subtotal">Total Price: $40.00</div>
+        <div className="cart-page__subtotal">Total Price: $ {sum}</div>
 
         <a href="/checkout" className="cart-page__checkout-btn">
           Proceed to Checkout <BiBadgeCheck />
