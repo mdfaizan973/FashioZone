8bd7ef6a65d7130788d8af51322bb763e7017a31
mdfaizan973 <faizan.md9735@gmail.com>
2023-06-26 23:52:46 +0530

    26 may end

--- fashion_zone/src/Pages/SignIn.jsx
+++ fashion_zone/src/Pages/SignIn.jsx
@@ -1,6 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Styles/Account.css";
+import { toast } from "react-toastify";
+import axios from "axios";
 export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const handleuser_submit = (e) => {
+    e.preventDefault();
+    // let obj = {
+    //   email,
+    //   pass,
+    // };
+    if (email == "eve.holt@reqres.in") {
+      fetch(`https://reqres.in/api/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, pass }),
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          console.log(res);
+          alert("Admin Login Success...");
+          window.location.href = "/"; //here should be admin
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      fetch("http://localhost:8888/users")
+        .then((response) => response.json())
+        .then((users) => {
+          const user = users.find(
+            (user) => user.email === email && user.pass === pass
+          );
+          if (user) {
+            toast.success("Login successful");
+            let st = setTimeout(() => {
+              window.location.href = "/";
+
+              return () => st;
+            }, 1000);
+          } else {
+            toast.error("Login Failed");
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    }
+  };
   return (
     <div>
       <div id="body">
@@ -9,13 +59,15 @@ export default function SignIn() {
 
           <h2 className="login-title">Login</h2>
 
-          <form className="login-form">
+          <form className="login-form" onSubmit={handleuser_submit}>
             <div className="form-group">
               <label for="username">Email address</label>
               <input
                 type="text"
                 id="username"
                 placeholder="Enter your username"
+                onChange={(e) => setEmail(e.target.value)}
+                required
               />
             </div>
 
@@ -25,6 +77,8 @@ export default function SignIn() {
                 type="password"
                 id="password"
                 placeholder="Enter your password"
+                onChange={(e) => setPass(e.target.value)}
+                required
               />
             </div>
 
--- fashion_zone/src/Pages/SignUp.jsx
+++ fashion_zone/src/Pages/SignUp.jsx
@@ -1,40 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Styles/Account.css";
+import axios from "axios";
+import { toast } from "react-toastify";
 export default function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const submituser = (e) => {
+    e.preventDefault();
+    let obj = {
+      name,
+      email,
+      pass,
+    };
+    axios
+      .post(`http://localhost:8888/users`, obj)
+      .then((res) => {
+        console.log(res);
+        toast.success("Account Created Successfully");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div>
       <div id="body">
-        <div className="login-container">
+        <div className="signin-container">
           <div className="login-logo"></div>
 
           <h2 className="login-title">Create Account</h2>
 
-          <form className="login-form">
+          <form className="login-form" onSubmit={submituser}>
             <div className="form-group">
-              <label for="username">Username</label>
+              <label htmlFor="username">Username</label>
               <input
                 type="text"
                 id="username"
                 placeholder="Enter your username"
+                onChange={(e) => setName(e.target.value)}
+                required
               />
             </div>
 
             <div className="form-group">
-              <label for="email">Email</label>
-              <input type="email" id="email" placeholder="Enter your Email" />
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Enter your Email"
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
             </div>
 
             <div className="form-group">
-              <label for="password">Password</label>
+              <label htmlFor="password">Password</label>
               <input
                 type="password"
                 id="password"
                 placeholder="Enter your password"
+                onChange={(e) => setPass(e.target.value)}
+                required
               />
             </div>
 
             <div className="form-group">
-              <input type="submit" value="Login" />
+              <input type="submit" value="Register" />
             </div>
           </form>
 
