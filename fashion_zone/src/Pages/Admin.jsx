efd10a974af24c463513a98a2ec5c82f628f90df
mdfaizan973 <faizan.md9735@gmail.com>
2023-06-27 23:45:23 +0530

    able to do2

--- fashion_zone/src/Pages/Admin.jsx
+++ fashion_zone/src/Pages/Admin.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+import { Link as RouterLink } from "react-router-dom";
 export default function Admin() {
   return (
     <div>
-      <h1>Admin</h1>
+      <RouterLink to="/faizan">
+        <h1 style={{ color: "red", fontSize: "5rem" }}>Admin ➡️ ➡️</h1>
+      </RouterLink>
     </div>
   );
 }
