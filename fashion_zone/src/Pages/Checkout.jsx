39a2adcf7a78ea367c3315d9d70d922fdd93d692
mdfaizan973 <faizan.md9735@gmail.com>
2023-06-27 15:31:48 +0530

    able to make bigger

--- fashion_zone/src/Pages/Checkout.jsx
+++ fashion_zone/src/Pages/Checkout.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function Checkout() {
-  return <div>Checkout</div>;
+  return (
+    <div>
+      <h1
+        style={{
+          color: "red",
+          textAlign: "center",
+          fontSize: "5rem",
+        }}
+      >
+        Checkout
+      </h1>
+    </div>
+  );
 }
