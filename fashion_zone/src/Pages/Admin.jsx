a636c7f56ce7ce70ff1cf0df78f56cd248c1c3a3
mdfaizan973 <faizan.md9735@gmail.com>
2023-07-01 21:46:22 +0530

    able to make UI of admin form

--- fashion_zone/src/Pages/Admin.jsx
+++ fashion_zone/src/Pages/Admin.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link as RouterLink } from "react-router-dom";
 import "./Styles/Admins.css";
+import Form from "./Admin/Form";
 export default function Admin() {
   return (
     <div>
@@ -8,19 +9,14 @@ export default function Admin() {
         <div id="sidebar">
           <h1>Admin Dashboard</h1>
           <ul>
-            <li class="active">Dashboard</li>
+            <li>Dashboard</li>
             <li>Users</li>
             <li>Products</li>
-            <li>Orders</li>
+            <li>Admin</li>
           </ul>
         </div>
         <div id="content">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae nihil
-          quos inventore quas consectetur necessitatibus, tempore distinctio
-          dolores. Reprehenderit commodi sint veniam fuga officiis eos
-          reiciendis ipsa quis temporibus repudiandae quibusdam odit asperiores
-          exercitationem dolorum officia eaque, sit corrupti iure, iste labore
-          ducimus vitae deleniti accusantium hic. Molestiae, temporibus quidem?
+          <Form />
         </div>
       </div>
     </div>
