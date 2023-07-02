c778157a068ba08f023e03f3db8574df3e596648
mdfaizan973 <faizan.md9735@gmail.com>
2023-07-02 18:39:22 +0530

    added ata

--- fashion_zone/src/Pages/Mens.jsx
+++ fashion_zone/src/Pages/Mens.jsx
@@ -20,7 +20,7 @@ export default function Mens() {
   const getData = (sear, page, filter) => {
     console.log(filter);
     axios
-      .get(`http://localhost:8888/mens_data?gender=mens`)
+      .get(`http://localhost:8080/web_data?gender=mens`)
       .then((res) => {
         setPagelength(res.data.length);
       })
@@ -29,7 +29,7 @@ export default function Mens() {
       });
     axios
       .get(
-        `http://localhost:8888/mens_data?gender=mens&_limit=${itemshow}&_page=${page}`
+        `http://localhost:8080/web_data?gender=mens&_limit=${itemshow}&_page=${page}`
       )
       .then((res) => {
         setProd(res.data);
